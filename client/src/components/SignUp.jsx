bde25e392e6a9df0043bd6fe90ccbfa60603e99c
Pappu Singh <pappu.singh159687@gmail.com>
2024-10-29 05:22:57 +0530

    changed login and sign-up backend url

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -46,7 +46,7 @@ const SignUp = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/v1/users/register",
+        "https://lifebahn-heaven-05-backend.vercel.app/api/v1/users/register",
         data,
         {
           headers: {
